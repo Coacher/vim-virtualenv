aa85d52e1d6cd846727bfdc8f1e7076f0801826d
Ilya Tumaykin <itumaykin@gmail.com>
2014-11-06 18:05:50 +0300

    python: do not change $PYTHONPATH

--- autoload/virtualenv/virtualenv.py
+++ autoload/virtualenv/virtualenv.py
@@ -10,35 +10,29 @@ if sys.version_info.major == 3:
 
 def virtualenv_is_armed():
     if (
-            ('__virtualenv_saved_sys_path' in globals()) and
-            ('__virtualenv_saved_os_path' in globals()) and
-            ('__virtualenv_saved_os_pythonpath' in globals())
+            ('__virtualenv_saved_sys_path' in globals())
+            and
+            ('__virtualenv_saved_os_path' in globals())
     ):
         print('armed')
+
 
 def virtualenv_activate(activate_this):
     global __virtualenv_saved_sys_path
     global __virtualenv_saved_os_path
-    global __virtualenv_saved_os_pythonpath
     __virtualenv_saved_sys_path = list(sys.path)
     __virtualenv_saved_os_path = os.environ.get('PATH', '')
-    __virtualenv_saved_os_pythonpath = os.environ.get('PYTHONPATH', '')
 
     execfile(activate_this, dict(__file__=activate_this))
 
-    # sys.path is replaced in activate_this.py, update PYTHONPATH accordingly
-    os.environ['PYTHONPATH'] = os.pathsep.join(list(sys.path))
 
 def virtualenv_deactivate():
     try:
         global __virtualenv_saved_sys_path
         global __virtualenv_saved_os_path
-        global __virtualenv_saved_os_pythonpath
         sys.path[:] = __virtualenv_saved_sys_path
         os.environ['PATH'] = __virtualenv_saved_os_path
-        os.environ['PYTHONPATH'] = __virtualenv_saved_os_pythonpath
         del __virtualenv_saved_sys_path
         del __virtualenv_saved_os_path
-        del __virtualenv_saved_os_pythonpath
     except NameError:
         pass
